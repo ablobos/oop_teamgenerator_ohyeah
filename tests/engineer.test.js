b2eb81f267f00e0ea8c172d07ec9c86ab24f82c1
Abdiel "A.B." Villalobos <ablobosportfolio@gmail.com>
2023-02-25 17:40:47 -0500

    fixed const Engineer to require from Engineer.js NOT Employee.js

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,12 +1,12 @@
 const inquirer = require('inquirer');
-const Engineer = require('../classes/Employee.js')
+const Engineer = require('../classes/Engineer.js')
 
 test('function should return expected output', async () => {
     const spy = jest.spyOn(inquirer, 'prompt')
     spy.mockImplementation(() => Promise.resolve({
         name: 'A.B. Lobos',
         email: 'ablobos@outlook.com',
-        github: 'ablobos'
+        github: 'abloboshub'
     }));
 
     const engineer = new Engineer()
@@ -19,12 +19,12 @@ test('function should return expected output', async () => {
 test('function should validate email property', async () => {
     const spy = jest.spyOn(inquirer, 'prompt')
     spy.mockImplementation(() => Promise.resolve({
-        github: 'ablobos',
+        github: 'ablobosgithub',
     }));
 
     const engineer = new Engineer()
     await engineer.askGitHub()
-    expect(engineer.github).toBe('ablobos')
+    expect(engineer.github).toBe('ablobosgithub')
 
     spy.mockRestore();
 })
